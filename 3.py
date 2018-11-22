ca5f538c71b4f202927c67953242b27423bf1a64
Zachary Weeden <zweeden@datto.com>
2018-11-22 15:54:58 -0500

    Some progress on 3 - put tree into dictionary

--- 3.py
+++ 3.py
@@ -18,6 +18,8 @@ The following test should pass:
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
 """
+import json
+
 
 class Node:
     def __init__(self, val, left=None, right=None):
@@ -27,10 +29,49 @@ class Node:
 
 
 def serialize(node):
-    pass
+    binary_tree_as_dict = {node.val: {'left': [], 'right': []}}
+    
+    current_node = node
+    try:
+        while current_node.val is not None:
+            binary_tree_as_dict[node.val]['left'].append(current_node.left)
+            binary_tree_as_dict[node.val]['right'].append(current_node.right)
+            current_node = current_node.left
+    except:
+        pass
+
+    current_node = node.right
+    try:
+        while current_node.val is not None:
+            binary_tree_as_dict[node.val]['left'].append(current_node.left)
+            binary_tree_as_dict[node.val]['right'].append(current_node.right)
+            current_node = current_node.right
+    except:
+        pass
+
+    for index, n in enumerate(binary_tree_as_dict[node.val]['left']):
+        if isinstance(n, Node):
+            binary_tree_as_dict[node.val]['left'][index] = n.val
+
+    for index, n in enumerate(binary_tree_as_dict[node.val]['right']):
+        if isinstance(n, Node):
+            binary_tree_as_dict[node.val]['right'][index] = n.val
+
+    print(json.dumps(binary_tree_as_dict, indent=4))
 
 def deserialize(serialized_node):
     pass
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'
+#        1
+#   2        3
+# 4   5        6
+#7
+node_7 = Node('left.left.left', None, None)
+node_4 = Node('left.left', node_7, None)
+node_6 = Node('right.right', None, None)
+node_5 = Node('left.right', None, None)
+node_3 = Node('right', None, node_6)
+node_2 = Node('left', node_4, node_5)
+node_1 = Node('root', node_2, node_3)
+serialize(node_1)
+#assert deserialize(serialize(node_1)).left.left.val == 'left.left'
